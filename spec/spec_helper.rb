985eb532f8e2a133fd8a387792bbb65f106a9b22
Nathaniel Bibler <git@nathanielbibler.com>
2012-03-07 15:59:52 -0500

    Add basic request redirection specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |support|
+    require support
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
